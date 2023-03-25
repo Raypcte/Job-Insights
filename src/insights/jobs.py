f6ed68fd61cac63f109ea19fc182eefb8828c800
Raypcte <rayanevalak@gmail.com>
2023-03-24 23:17:50 -0300

    correcao

--- src/insights/jobs.py
+++ src/insights/jobs.py
@@ -13,10 +13,14 @@ def read(path: str) -> List[Dict]:
 
     """Reads a file from a given path and returns its contents
     Parameters
+    ----------
+    path : str
+        Full path to file
+    Returns
+    -------
     list
         List of rows as dicts
     """
-
     raise NotImplementedError
 
 
@@ -28,33 +32,27 @@ def get_unique_job_types(path: str) -> List[str]:
     return job_types
 
     """Checks all different job types and returns a list of them
-
     Must call `read`
-
     Parameters
     ----------
     path : str
         Must be passed to `read`
-
     Returns
     -------
     list
         List of unique job types
-
     """
+    raise NotImplementedError
 
 
 def filter_by_job_type(jobs: List[Dict], job_type: str) -> List[Dict]:
-
     """Filters a list of jobs by job_type
-
     Parameters
     ----------
     jobs : list
         List of jobs to be filtered
     job_type : str
         Job type for the list filter
-
     Returns
     -------
     list
