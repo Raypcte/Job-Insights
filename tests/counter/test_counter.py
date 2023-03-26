58ecc60623a6ca4c555393189074fe6a44754476
Raypcte <rayanevalak@gmail.com>
2023-03-26 19:16:30 -0300

    req10

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,7 @@
-# from src.pre_built.counter import count_ocurrences
+from src.pre_built.counter import count_ocurrences
 
 
 def test_counter():
     pass
+    assert count_ocurrences("data/jobs.csv", "Xablau") == 0
+    assert count_ocurrences("data/jobs.csv", "Javascript") == 122
